bd299d610644b2b76617fe41bca771c2d2bff4c4
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-13 13:08:48 -0700

    Add notes, user stories, and constructor function in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 // add event listeners
+
+// track amount of clicks
+// track # times displayed
+// Upon receiving a click, three new non-duplicating random images need to be automatically displayed. In other words, the three images that are displayed should contain no duplicates, nor should they duplicate with any images that we displayed immediately before.
+
+function Item(filepath, displayName, id) {
+  this.filepath = filepath;
+  this.displayName = displayName;
+  this.id = id;
+  this.votes = 0;
+  this.views = 0;
+
+  Item.allItems.push(this);
+}
+
+Item.allItems = [];
+
+
+// you'll want a constructor function that creates an object associated with each image, and has (at a minimum) properties for the name of the image (to be used for display purposes), its filepath, the number of times it has been shown, and the number of times it has been clicked. You'll probably find it useful to create a property that contains a text string you can use as an ID in HTML.
+
+// After 25 selections have been made, turn off the event listeners on the images (to prevent additional voting) and also display a list of the products with votes received with each list item looking like "3 votes for the Banana Slicer".
--- user-stories.md
+++ user-stories.md
@@ -1,3 +1,10 @@
 # User Stories
 
 > Format: As a X, I want to Y in order to Z.
+
+* As a goods provider, I want to track which products are popular among my customers in order to keep those items in stock.
+* As a customer, I want to select items I like in order to know where my priorities are.
+* As a developer, I want to display these items largely so that my users don't struggle to decide which item they prefer.
+* As a goods provider, I want to show my customer data on what items they selected so that it gives them insight into their preferences.
+* As a developer, I want to make sure I show a variety of images and make sure none of them are displayed consecutively so that each image gets a fair amount of screen time.
+* As as customer, I want to see a variety of products in order to have more options.
